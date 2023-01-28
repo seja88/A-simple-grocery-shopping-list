5c8440f1f0b29b447e62be9866ff4784fe9b5192
Jan Sebesta <jan.sebesta88@gmail.com>
2023-01-28 20:01:22 +0100

    kinda finished

--- js/script.js
+++ js/script.js
@@ -10,16 +10,103 @@
 // 8.Set this content as innerHTML of the #list element.
 
 //2.
-const groceryItems = ['Apple', 'Oranges','Bananas', 'Milk', 'Protein_bar']
+// const groceryItems = ['Apple', 'Oranges','Bananas', 'Milk', 'Protein_bar']
 //4.
+// const newDiv = document.getElementById('list')
+
+// groceryItems.forEach(item => {
+//     const div = document.createElement('div')
+//     newDiv.appendChild(div)
+//     div.classList.add('item')
+//     div.innerHTML += item
+// });
+//STAGE 2
+
+// const newDiv = document.getElementById('list')
+// const groceryItems = {
+//     name: ['Apple', 'Oranges','Bananas', 'Milk', 'Protein_bar'],
+//     amount: [4,6,2,1,1]
+// }
+
+// for(let i = 0; i < groceryItems.name.length; i++) {
+//     const divName = document.createElement('div')
+//     const divAmount = document.createElement('div')
+//     newDiv.appendChild(divName)
+//     newDiv.appendChild(divAmount)
+//     divName.classList.add(groceryItems.name[i])
+//     divName.innerHTML += groceryItems.name[i]
+//     divAmount.innerHTML += groceryItems.amount[i]
+// }
+
+//STAGE 3
+
 const newDiv = document.getElementById('list')
+const select = document.createElement('select')
+newDiv.appendChild(select)
+select.id = 'selectProduct'
 
-groceryItems.forEach(item => {
-    const div = document.createElement('div')
-    newDiv.appendChild(div)
-    div.classList.add('item')
-    div.innerHTML += item
-});
+const option1 = document.createElement('option')
+select.appendChild(option1)
+option1.textContent = 'Apple'
+option1.value = 'apple'
 
+const option2 = document.createElement('option')
+select.appendChild(option2)
+option2.textContent = 'Banana'
+option1.value = 'banana'
+
+const option3 = document.createElement('option')
+select.appendChild(option3)
+option3.textContent = 'Mango'
+option1.value = 'mango'
+
+const option4 = document.createElement('option')
+select.appendChild(option4)
+option4.textContent = 'Pineapple'
+option1.value = 'pineapple'
+
+// const userProduct = document.createElement('input')
+// newDiv.appendChild(userProduct)
+// userProduct.placeholder = 'Product Name'
+const userAmount = document.createElement('input')
+newDiv.appendChild(userAmount)
+userAmount.placeholder = 'How many?'
+userAmount.type = 'number'
+userAmount.id = 'userAmount'
+const button = document.createElement('button')
+newDiv.appendChild(button)
+button.textContent = 'ADD PRODUCT'
+button.id = 'btn'
+const wrap = document.createElement('div')
+document.body.appendChild(wrap)
+wrap.style.display = 'flex'
+wrap.style.flexDirection = 'column'
+const listDiv = document.createElement('div')
+wrap.appendChild(listDiv)
+listDiv.style.display = 'flex'
+listDiv.classList.add('product')
+listDiv.style.flexDirection = 'column'
+
+const deleteButton = document.createElement('button')
+//button event
+button.addEventListener('click', () => {
+    let userAmount = document.getElementById('userAmount').value
+    let userProduct = document.getElementById('selectProduct').value
+    const product = document.createElement('div')
+    listDiv.appendChild(product)
+    product.innerHTML = userProduct
+    const amount = document.createElement('div')
+    listDiv.appendChild(amount)
+    amount.innerHTML = userAmount
+    const deleteButton = document.createElement('button')
+    listDiv.appendChild(deleteButton)
+    deleteButton.textContent = 'X'
+    deleteButton.style.width = '5em'
+    deleteButton.addEventListener('click', () => {
+        product.style.display = 'none'
+        amount.style.display = 'none'
+        deleteButton.style.display = 'none'
+    })
+})
 
 
